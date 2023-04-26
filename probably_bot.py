0a3b1c2fea75b091dec4b48d86e998f96667bf06
derekjamerson <derekjamerson@gmail.com>
2023-04-26 03:02:59 -0400

    testing pre-commit

--- probably_bot.py
+++ probably_bot.py
@@ -1,23 +1,17 @@
 # bot.py
 import os
 import random
-import sqlite3
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-
-from table2ascii import Alignment, table2ascii as t2a, PresetStyle
-
-from oauth2client.service_account import ServiceAccountCredentials
-
+from table2ascii import Alignment, PresetStyle
+from table2ascii import table2ascii as t2a
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-intents=discord.Intents.all()
+intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
-
-
 
 
 @bot.command(name='customs', help='Creates 2 teams of 5')
@@ -26,7 +20,7 @@ async def customs(ctx):
 
     if not voice_state:
         return
-    
+
     member_names = [x.name for x in voice_state.channel.voice_members]
     members_count = len(member_names)
 
@@ -36,10 +30,10 @@ async def customs(ctx):
     if members_count > 10:
         await ctx.channel.send('Too many niggas!')
         return
-    
+
     random.shuffle(member_names)
-    team_one = member_names[:members_count//2]
-    team_two = member_names[members_count//2:]
+    team_one = member_names[: members_count // 2]
+    team_two = member_names[members_count // 2 :]
 
     team_one_text = '\n'.join(team_one)
     team_two_text = '\n'.join(team_two)
@@ -84,7 +78,6 @@ async def customs(ctx):
 #     output = get_table(headers, body)
 
 #     await ctx.send(f"```\n{title}\n\n{output}\n```")
-
 
 
 # @bot.command(name='win', help='Records winner of game')
@@ -138,19 +131,20 @@ async def customs(ctx):
 
 def get_table(headers, body):
     return t2a(
-            header=headers,
-            body=body,
-            style=PresetStyle.simple,
-            alignments=[Alignment.LEFT, Alignment.RIGHT, Alignment.LEFT, Alignment.CENTER],
-        )
+        header=headers,
+        body=body,
+        style=PresetStyle.simple,
+        alignments=[Alignment.LEFT, Alignment.RIGHT, Alignment.LEFT, Alignment.CENTER],
+    )
+
 
 def calc_win_rate(wins, games):
     wins = int(wins)
     games = int(games)
     if not games:
         return -1
-    
-    return wins/games
+
+    return wins / games
 
 
 def get_intro(member_names):
@@ -176,9 +170,8 @@ def get_intro(member_names):
         'This is gonna be a baka-laka-ham-dak',
         'Turn these idiots into CONVERTIBLES',
         "Show 'em their shit don't fit in your pocket",
-        'Put down the bananas so you can play the game.',
         f'{random.choice(range(5, 15))}. Mid 0 Vision Score',
-        ]
+    ]
     return random.choice(options)
 
 
