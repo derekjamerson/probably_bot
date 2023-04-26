549b1407ce0cee49fd450ee439b97363938619c1
derekjamerson <derekjamerson@gmail.com>
2023-04-26 03:13:08 -0400

    update create table

--- create_table.py
+++ create_table.py
@@ -1,20 +1,19 @@
 import sqlite3
 
 
-
 def create_table():
     conn = sqlite3.connect('./sqlite.db')
     cursor = conn.cursor()
     table = """
-        CREATE TABLE `customs_tracker` (
-            `ID` INT(10) NOT NULL AUTO_INCREMENT,
-            `Name` VARCHAR(50) NOT NULL,
-            `Wins` INT(10) NOT NULL DEFAULT '0',
-            `Games` INT(10) NOT NULL DEFAULT '0',
-            PRIMARY KEY (`ID`)
+        CREATE TABLE customs_tracker (
+            ID INTEGER PRIMARY_KEY,
+            Name VARCHAR(50) NOT NULL,
+            Wins INT(10) NOT NULL DEFAULT 0,
+            Games INT(10) NOT NULL DEFAULT 0
         );
     """
     cursor.execute(table)
+
 
 if __name__ == '__main__':
     create_table()
